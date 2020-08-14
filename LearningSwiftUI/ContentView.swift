c1a0a8982effc4a477d74480d582c7120e47ef25
Chuck Perdue <syclonefx@Chucks-MacBook-Pro.local>
2020-08-14 19:24:15 -0400

    initial commit

---
+++ LearningSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningSwiftUI
+//
+//  Created by Chuck Perdue on 8/14/20.
+//  Copyright © 2020 SycloneFX, LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
